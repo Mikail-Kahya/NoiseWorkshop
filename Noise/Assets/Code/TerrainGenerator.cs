7b0e8e6e95573afe6ccc4a15e9d8d1d9bf70e912
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2025-04-25 15:20:17 +0200

    feat: fBm with octaves

--- Noise/Assets/Code/TerrainGenerator.cs
+++ Noise/Assets/Code/TerrainGenerator.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 public class TerrainGenerator : MonoBehaviour
 {
     public ComputeShader computeShader;
+    // Coord
     public int width = 1024;
     public int height = 1024;
+    [Range(0.01f, 100.0f)]
     public float noiseScale = 10.0f;
+
+
+    [Range(1, 15)]
+    public int octaveCount = 6;
+    public float lacunarity = 2.0f;
+    public float gain = 1.0f;
+    
+    // Swapping
     public bool useQuinticInterpolation = true;
 
     public enum NoiseType
@@ -45,9 +55,14 @@ public class TerrainGenerator : MonoBehaviour
         computeShader.SetInt("NoiseWidth", width);
         computeShader.SetInt("NoiseHeight", height);
         computeShader.SetFloat("NoiseScale", noiseScale);
+
+        computeShader.SetInt("OctaveCount", octaveCount);
+        computeShader.SetFloat("Lacunarity", lacunarity);
+        computeShader.SetFloat("Gain", gain);
+
         computeShader.SetBool("NoiseQuinticInterpolation", useQuinticInterpolation);
         computeShader.SetInt("NoiseType", (int) noiseType);
-        computeShader.SetInt("HashType", (int) hashType);
+        computeShader.SetInt("HashType", (int)hashType);
 
         // threadSizeZ is not implements as it's not needed right now
         computeShader.Dispatch(0, (int)(width / threadSizeX), (int)(height / threadSizeY), 1);
