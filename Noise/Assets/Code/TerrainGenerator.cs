2a086e4241488e7fe9f66a5c7b06b980c8b3a7a3
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2025-04-25 12:22:41 +0200

    feat: simple noise

--- Noise/Assets/Code/TerrainGenerator.cs
+++ Noise/Assets/Code/TerrainGenerator.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public ComputeShader computeShader;
+    public int width = 1024;
+    public int height = 1024;
+
+    public float noiseScale = 10.0f;
+
+    private RenderTexture noise_textute;
+    private uint threadSizeX;
+    private uint threadSizeY;
+    private uint threadSizeZ;
+
     void Start()
     {
-        
+        noise_textute = new RenderTexture(width, height, 0, RenderTextureFormat.RHalf, RenderTextureReadWrite.Linear);
+        noise_textute.enableRandomWrite = true;
+        noise_textute.Create();
+
+        computeShader.GetKernelThreadGroupSizes(0, out threadSizeX, out threadSizeY, out threadSizeZ);
+        MeshRenderer mesh_renderer = GetComponent<MeshRenderer>();
+        mesh_renderer.material.SetTexture("_Noise", noise_textute);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        computeShader.SetTexture(0, "Noise", noise_textute);
+        computeShader.SetInt("NoiseWidth", width);
+        computeShader.SetInt("NoiseHeight", height);
+        computeShader.SetFloat("NoiseScale", noiseScale);
+
+        // threadSizeZ is not implements as it's not needed right now
+        computeShader.Dispatch(0, (int)(width / threadSizeX), (int)(height / threadSizeY), 1);
     }
 }
